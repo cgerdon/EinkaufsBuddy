cee8b4126340d94598992905e1eef10ec002d15c
Christoph <Christoph@Christoph-Pad.local>
2015-03-01 14:50:19 +0100

    Rating Gedöns; Work in progress...

---
+++ EinkaufsBuddy/src/Beans/RatingResults.java
@@ -0,0 +1,51 @@
+package Beans;
+
+
+public class RatingResults {
+
+	private int id;
+	private int buyerid;
+	private int advertiserid;
+	private int rating;
+	private String text;
+	private int adid;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getBuyerid() {
+		return buyerid;
+	}
+	public void setBuyerid(int buyerid) {
+		this.buyerid = buyerid;
+	}
+	public int getAdvertiserid() {
+		return advertiserid;
+	}
+	public void setAdvertiserid(int advertiserid) {
+		this.advertiserid = advertiserid;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public int getAdid() {
+		return adid;
+	}
+	public void setAdid(int adid) {
+		this.adid = adid;
+	}
+	
+	
+
+}
