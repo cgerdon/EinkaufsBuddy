570871fc35df060babbbc04b3a5b5a6600366943
Christoph <cgerdon@gmail.com>
2015-03-02 16:53:39 +0100

    Rating^2

--- EinkaufsBuddy/src/Beans/userView.java
+++ EinkaufsBuddy/src/Beans/userView.java
@@ -1,6 +1,7 @@
 package Beans;  
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,10 +19,150 @@ import javax.sql.DataSource;
 public class userView{  
 	
 	private int id;
-    DataSource ds;  
+	private String name;
+	private String last_name;
+	private Date birthdate;
+	private int car;
+	private String abouttext;
+	private int sex;
+	private String street;
+	private int plz;
+	private String phone;
+	private int[][] daytimeavailable;
+	
+	
+	
+    public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+
+	public int getCar() {
+		return car;
+	}
+
+
+	public void setCar(int car) {
+		this.car = car;
+	}
+
+
+	public String getAbouttext() {
+		return abouttext;
+	}
+
+
+	public void setAbouttext(String abouttext) {
+		this.abouttext = abouttext;
+	}
+
+
+	public int getSex() {
+		return sex;
+	}
+
+
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+
+
+	public String getStreet() {
+		return street;
+	}
+
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+
+	public int getPlz() {
+		return plz;
+	}
+
+
+	public void setPlz(int plz) {
+		this.plz = plz;
+	}
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+	public int[][] getDaytimeavailable() {
+		return daytimeavailable;
+	}
+
+
+	public void setDaytimeavailable(int[][] daytimeavailable) {
+		this.daytimeavailable = daytimeavailable;
+	}
+
+
+	DataSource ds;  
     
     public String showProfil(int id){
+    	//Oh gott, bitte muss ich die NIE wieder anschauen. 
     	this.id = id;
+    	 PreparedStatement ps = null;  
+         Connection con = null;  
+         ResultSet rs = null;  
+         if (ds != null) {  
+             try {  
+                 con = ds.getConnection();  
+                 if (con != null) {  
+                     String sql = "select id, name, last_name, birthdate, car, abouttext, fk_sex, street, plz, phone from member where id = '"  
+                             + id + "'";  
+                     ps = con.prepareStatement(sql);  
+                     rs = ps.executeQuery();  
+                     rs.next();  
+                     name = rs.getString("name");
+                     last_name = rs.getString("last_name");  
+                     birthdate = rs.getDate("birthdate");
+                     car = rs.getInt("car");
+                     abouttext = rs.getString("abouttext");
+                     street = rs.getString("street");
+                     plz = rs.getInt("plz");
+                     phone = rs.getString("phone");
+                 }  
+             } catch (SQLException sqle) {  
+                 sqle.printStackTrace();  
+             }  
+         }  
+     
 		return "fremdprofil";
     	
     }
@@ -46,43 +187,5 @@ public class userView{
 		this.id = id;
 	}
 	
-  
-    public void dbData(String uName) {  
-        if (uName != null) {  
-            PreparedStatement ps = null;  
-            Connection con = null;  
-            ResultSet rs = null;  
-  
-            if (ds != null) {  
-                try {  
-                    con = ds.getConnection();  
-                    if (con != null) {  
-                        String sql = "select id, mail, password_hash, name, last_name, birthdate, car, abouttext, fk_sex, street, plz, phone from member where mail = '"  
-                                + uName + "'";  
-                        ps = con.prepareStatement(sql);  
-                        rs = ps.executeQuery();  
-                        rs.next();  
-//                        dbName = rs.getString("mail");  
-//                        dbPassword = rs.getString("password_hash");  
-//                        id = rs.getInt("id");
-//                        firstName = rs.getString("name");
-//                        lastName = rs.getString("last_name");  
-//                        email = rs.getString("mail");
-//                       // password = rs.getString("password_hash");
-//                        birthday = rs.getDate("birthdate");
-//                        car = rs.getInt("car");
-//                        abouttext = rs.getString("abouttext");
-//                        street = rs.getString("street");
-//                        plz = rs.getInt("plz");
-//                        phone = rs.getString("phone");
-                    }  
-                } catch (SQLException sqle) {  
-                    sqle.printStackTrace();  
-                }  
-            }  
-        }
-    }  
-	
-
     
 }  
