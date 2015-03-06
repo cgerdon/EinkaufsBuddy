3fa7c1ba49984102424d6ef8b5949141b0af6614
Christoph <cgerdon@gmail.com>
2015-03-06 14:25:41 +0100

    Versuche den File Upload zu machen...

---
+++ EinkaufsBuddy/src/Beans/FileUploadValidator.java
@@ -0,0 +1,56 @@
+package Beans;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import javax.servlet.http.Part;
+ 
+@FacesValidator("FileUploadValidator")
+public class FileUploadValidator implements Validator {
+ 
+	@Override
+	public void validate(FacesContext context, UIComponent uiComponent,
+			Object value) throws ValidatorException {
+ 
+		Part part = (Part) value;
+ 
+		// 1. validate file name length
+		String fileName = getFileName(part);
+		System.out.println("----- validator fileName: " + fileName);
+		if(fileName.length() == 0 ) {
+			FacesMessage message = new FacesMessage("Error: File name is invalid !!");
+			throw new ValidatorException(message);
+		} else if (fileName.length() > 50) {
+			FacesMessage message = new FacesMessage("Error: File name is too long !!");
+			throw new ValidatorException(message);
+		}
+ 
+		// 2. validate file type (only text files allowed)
+		if (!"text/plain".equals(part.getContentType())) {
+			FacesMessage message = new FacesMessage("Error: File type is invalid !!");
+			throw new ValidatorException(message);
+		  }
+ 
+		// 3. validate file size (should not be greater than 512 bytes)
+		if (part.getSize() > 512) {
+			FacesMessage message = new FacesMessage("Error: File size is too big !!");
+			throw new ValidatorException(message);
+		}
+	}
+ 
+	// Extract file name from content-disposition header of file part
+	private String getFileName(Part part) {
+		final String partHeader = part.getHeader("content-disposition");
+		System.out.println("----- validator partHeader: " + partHeader);
+		for (String content : part.getHeader("content-disposition").split(";")) {
+			if (content.trim().startsWith("filename")) {
+				return content.substring(content.indexOf('=') + 1).trim()
+						.replace("\"", "");
+			}
+		}
+		return "";
+	}
+}
--- EinkaufsBuddy/src/Beans/User.java
+++ EinkaufsBuddy/src/Beans/User.java
@@ -5,6 +5,16 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
+ 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.servlet.http.Part;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -47,9 +57,78 @@ public class User implements Serializable{
 	private String phone;
 	private String dbPassword;
 	private String dbName;
+	private Part part;
+	private String statusMessage;
+
 	private boolean[][] daytimeavailable;
 	DataSource ds;
-
+	
+	public String uploadFile() throws IOException {
+		 
+		// Extract file name from content-disposition header of file part
+		String fileName = getFileName(part);
+		System.out.println("***** fileName: " + fileName);
+ 
+		String basePath = "C:" + File.separator + "temp" + File.separator;
+		File outputFilePath = new File(basePath + fileName);
+ 
+		// Copy uploaded file to destination path
+		InputStream inputStream = null;
+		OutputStream outputStream = null;
+		try {
+			inputStream = part.getInputStream();
+			outputStream = new FileOutputStream(outputFilePath);
+ 
+			int read = 0;
+			final byte[] bytes = new byte[1024];
+			while ((read = inputStream.read(bytes)) != -1) {
+				outputStream.write(bytes, 0, read);
+			}
+ 
+			statusMessage = "File upload successfull !!";
+		} catch (IOException e) {
+			e.printStackTrace();
+			statusMessage = "File upload failed !!";
+		} finally {
+			if (outputStream != null) {
+				outputStream.close();
+			}
+			if (inputStream != null) {
+				inputStream.close();
+			}
+		}
+		return null;    // return to same page
+	}
+ 
+	public Part getPart() {
+		return part;
+	}
+ 
+	public void setPart(Part part) {
+		this.part = part;
+	}
+ 
+	public String getStatusMessage() {
+		return statusMessage;
+	}
+ 
+	public void setStatusMessage(String statusMessage) {
+		this.statusMessage = statusMessage;
+	}
+ 
+	// Extract file name from content-disposition header of file part
+	private String getFileName(Part part) {
+		final String partHeader = part.getHeader("content-disposition");
+		System.out.println("***** partHeader: " + partHeader);
+		for (String content : part.getHeader("content-disposition").split(";")) {
+			if (content.trim().startsWith("filename")) {
+				return content.substring(content.indexOf('=') + 1).trim()
+						.replace("\"", "");
+			}
+		}
+		return null;
+	}
+	
 	public void onDateSelect(SelectEvent event) {
 		FacesContext facesContext = FacesContext.getCurrentInstance();
 		SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
