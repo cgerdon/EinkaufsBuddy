857c697c878a2beea0ea023877ee03278bcae7ea
Christoph <cgerdon@gmail.com>
2015-03-09 10:27:35 +0100

    Kontakt Mail Versand

--- EinkaufsBuddy/src/Beans/User.java
+++ EinkaufsBuddy/src/Beans/User.java
@@ -16,12 +16,20 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 //import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -62,6 +70,96 @@ public class User implements Serializable {
 	private String dbPassword;
 	private String dbName;
 	private StreamedContent dbImage;
+	
+
+	private String sender;
+	private String titel;
+	private String text;
+	
+	public String getSender() {
+		return sender;
+	}
+
+
+
+
+
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+
+
+
+
+
+	public String getTitel() {
+		return titel;
+	}
+
+
+
+
+
+	public void setTitel(String titel) {
+		this.titel = titel;
+	}
+
+
+
+
+
+	public String getText() {
+		return text;
+	}
+
+
+
+
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+
+
+
+
+	public String SendMail(){
+
+	 final String username = "einkaufsbuddy@gmail.com";
+     final String password = "buddydeseinkaufs";
+
+     Properties props = new Properties();
+     props.put("mail.smtp.starttls.enable", "true");
+     props.put("mail.smtp.auth", "true");
+     props.put("mail.smtp.host", "smtp.gmail.com");
+     props.put("mail.smtp.port", "587");
+
+     Session session = Session.getInstance(props,
+       new javax.mail.Authenticator() {
+         protected PasswordAuthentication getPasswordAuthentication() {
+             return new PasswordAuthentication(username, password);
+         }
+       });
+
+     try {
+
+         Message message = new MimeMessage(session);
+         message.setFrom(new InternetAddress(sender));
+         message.setRecipients(Message.RecipientType.TO,
+             InternetAddress.parse("einkaufsbuddy@gmail.com"));
+         message.setSubject(titel);
+         message.setText(text);
+
+         Transport.send(message);
+
+         System.out.println("E-Mail erfolgreich versendet!");
+
+     } catch (MessagingException e) {
+         throw new RuntimeException(e);
+     }
+     return "home?faces-redirect=true";
+ }
 
 	public StreamedContent getDbImage() {
 		return dbImage;
